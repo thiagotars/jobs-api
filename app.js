3350d970a4309db0328b1d9982ceab1a7bc6d24f
Thiago Tarsitano <105082110+thiagotars@users.noreply.github.com>
2023-09-05 14:14:40 -0300

    Add a get request to the root path

--- app.js
+++ app.js
@@ -32,6 +32,9 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get("/", (req, res) => {
+  res.send("Jobs API");
+});
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
